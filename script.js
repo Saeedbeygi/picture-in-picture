0692d40cb5bea558fc8329feaaf8a1d26ac3de41
SAEED MIRZABEYGI <92190197+Saeedbeygi@users.noreply.github.com>
2021-10-29 22:02:54 +1000

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia(); 
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+
+        }    
+    } catch (error) {
+        //catch error here
+        console.log('whoops, error here:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+// Disable Button
+button.disable = true;
+// Start Picture in Picture
+await videoElement.requestPictureInPicture();
+// Rest Button
+button.disable = false;
+});
+// On Load
+selectMediaStream();
